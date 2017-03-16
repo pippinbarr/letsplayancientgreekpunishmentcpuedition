fe51a5ec679284ab1d01ce67ae336339e2ec0bd7
Pippin Barr <pippin.barr@gmail.com>
2017-03-16 13:34:56 -0400

    Clawed my way back to basic Prometheus sprites and animations and texture atlas

--- js/Preloader.js
+++ js/Preloader.js
@@ -1,8 +1,8 @@
 
 BasicGame.Preloader = function (game) {
 
-	this.background = null;
-	this.preloadBar = null;
+	// this.background = null;
+	// this.preloadBar = null;
 
 	this.ready = false;
 
@@ -22,33 +22,24 @@ BasicGame.Preloader.prototype = {
 		//	as the files below are loaded in.
 		// this.load.setPreloadSprite(this.preloadBar);
 
-		//	Here we load the rest of the assets our game needs.
-		//	As this is just a Project Template I've not provided these assets, swap them for your own.
-		// this.load.image('titlepage', 'images/title.jpg');
-		// this.load.atlas('playButton', 'images/play_button.png', 'images/play_button.json');
-		// this.load.audio('titleMusic', ['audio/main_menu.mp3']);
-		// this.load.bitmapFont('caslon', 'fonts/caslon.png', 'fonts/caslon.xml');
-		//	+ lots of other required assets here
+		// this.load.spritesheet('prometheus', 'assets/sprites/prometheus/prometheus.png', 21, 10);
+		// this.load.spritesheet('eagle', 'assets/sprites/prometheus/eagle.png', 12, 11);
+		// this.load.spritesheet('rock', 'assets/sprites/prometheus/rockandchains.png',200,100);
+		// this.load.spritesheet('bg', 'assets/sprites/prometheus/bg.png',2,1);
 
+		// this.game.load.json('sprites', 'assets/atlas/atlas.json');
+		// this.load.atlasJSONHash('atlas','assets/atlas/atlas.png',"assets/atlas.json",null);
+
+this.game.load.atlas('atlas', 'assets/atlas/assets.png', 'assets/atlas/assets.json');
 	},
 
 	create: function () {
 
 		//	Once the load has finished we disable the crop because we're going to sit in the update loop for a short while as the music decodes
 		// this.preloadBar.cropEnabled = false;
-
 	},
 
 	update: function () {
-
-		//	You don't actually need to do this, but I find it gives a much smoother game experience.
-		//	Basically it will wait for our audio file to be decoded before proceeding to the MainMenu.
-		//	You can jump right into the menu if you want and still play the music, but you'll have a few
-		//	seconds of delay while the mp3 decodes - so if you need your music to be in-sync with your menu
-		//	it's best to wait for it to decode here first, then carry on.
-
-		//	If you don't have any music in your game then put the game.state.start line into the create function and delete
-		//	the update function completely.
 
 		// if (this.cache.isSoundDecoded('titleMusic') && this.ready == false)
 		if (this.ready == false)
--- js/Prometheus.js
+++ js/Prometheus.js
@@ -7,6 +7,40 @@ BasicGame.Prometheus.prototype = {
 
     create: function () {
 
+      this.stage.smoothed = false;
+
+      this.stage.backgroundColor = 0xcccccc;
+
+      this.bg = this.add.sprite(0,0,'atlas');
+      this.bg.animations.add('day',Phaser.Animation.generateFrameNames('prometheus/bg_', 1, 1, '.png'), 5, true);
+      this.bg.animations.add('night',Phaser.Animation.generateFrameNames('prometheus/bg_', 2, 2, '.png'), 5, true);
+      scale(this.bg,400);
+
+      this.bg.animations.play('day');
+
+      this.prometheus = this.add.sprite(360,220,'atlas');
+      this.prometheus.animations.add('idle',Phaser.Animation.generateFrameNames('prometheus/prometheus_', 1, 1, '.png'), 5, true);
+      this.prometheus.animations.add('struggle',Phaser.Animation.generateFrameNames('prometheus/prometheus_', 1, 2, '.png'), 5, true);
+      this.prometheus.animations.add('nighttime',Phaser.Animation.generateFrameNames('prometheus/prometheus_', 3, 3, '.png'), 5, true);
+      scale(this.prometheus,4);
+
+      this.prometheus.animations.play('idle');
+
+      this.rock = this.add.sprite(0,0,'atlas');
+      this.rock.animations.add('day',Phaser.Animation.generateFrameNames('prometheus/rock_', 1, 1, '.png'), 5, true);
+      this.rock.animations.add('night',Phaser.Animation.generateFrameNames('prometheus/rock_', 2, 2, '.png'), 5, true);
+      scale(this.rock,4);
+
+      this.rock.animations.play('day');
+
+      this.eagle = this.add.sprite(100,100,'atlas');
+      this.eagle.animations.add('flying',Phaser.Animation.generateFrameNames('prometheus/eagle_', 1, 4, '.png'), 5, true);
+      this.eagle.animations.add('perched',Phaser.Animation.generateFrameNames('prometheus/eagle_', 5, 5, '.png'), 5, true);
+      this.eagle.animations.add('peck',Phaser.Animation.generateFrameNames('prometheus/eagle_', 6, 5, '.png'), 5, false);
+      scale(this.eagle,4);
+
+      this.eagle.animations.play('flying');
+
 
     },
 
@@ -22,3 +56,11 @@ BasicGame.Prometheus.prototype = {
     }
 
 };
+
+
+function scale (sprite,amount) {
+  sprite.anchor.x = 0;
+  sprite.anchor.y = 0;
+  sprite.scale.x *= amount;
+  sprite.scale.y *= amount;
+}
